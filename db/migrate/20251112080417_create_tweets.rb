e00fe7583a31945df93820115d1f3958679c504e
Yudai Inoue <2311140038t@ed.fuk.kindai.ac.jp>
2025-11-12 08:35:39 +0000

    model2

--- db/migrate/20251112080417_create_tweets.rb
+++ db/migrate/20251112080417_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration[8.0]
   def change
     create_table :tweets do |t|
       t.string :message
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_12_080417) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_12_082350) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
