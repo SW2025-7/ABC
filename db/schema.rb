15893952631d7c06371535053e14d3a0273ddfd9
Yudai Inoue <2311140038t@ed.fuk.kindai.ac.jp>
2026-01-20 12:48:09 +0000

    push to render.com

--- db/schema.rb
+++ db/schema.rb
@@ -45,15 +45,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_03_074844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes_count", default: 0
-<<<<<<< HEAD
-    t.integer "user_id"
-=======
->>>>>>> 77079ee (push to render.com)
   end
 
   create_table "users", force: :cascade do |t|
     t.string "uid"
-    t.string "pass"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
